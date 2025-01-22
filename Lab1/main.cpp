714144d2c5a010c888b0aceeb25da228e16a3fd0
kelseylw1822 <kelseylw1822@gmail.com>
2025-01-22 13:54:14 -0500

    updated areaCalc

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,14 +1,49 @@
-//
-//  main.cpp
-//  Lab1
-//
-//  Created by Kelsey Wessel on 1/22/25.
-//
+// My Name: Kelsey Wessel
+// My Class: CPSC 1021
+// Date: 01/22/2025
+// Desc: Calculate the area of a square, circle, or right triangle based on user input
+// Time: About 10 minutes
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    const double PI = 3.14159;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int input;
+    double area, sidelength, radius, base, height;
+    cout << "Program to calculate areas of objects\n1 -- square\n2 -- circle\n3 -- right triangle\n4 -- quit" << endl;
+    cin >> input;
+
+    if (input == 1) {
+        cout << "Side length of the square: ";
+        cin >> sidelength;
+        area = sidelength * sidelength;
+        cout << "Area = " << fixed << setprecision(2) << area << endl;
+        return 0;
+    }
+    else if (input == 2) {
+        cout << "Radius of the circle: ";
+        cin >> radius;
+        area = (radius * radius) * PI;
+        cout << "Area = " << fixed << setprecision(2) << area << endl;
+        return 0;
+    }
+    else if (input == 3) {
+        cout << "Base of the triangle: ";
+        cin >> base;
+        cout << "Height of the triangle: ";
+        cin >> height;
+        area = (base * height)/2;
+        cout << "Area = " << fixed << setprecision(2) << area << endl;
+        return 0;
+    }
+    else if (input == 4) {
+        return 0;
+    }
+    else {
+        cout << "You entered an invalid choice. Good bye!" << endl;
+        return 0;
+    }
 }
