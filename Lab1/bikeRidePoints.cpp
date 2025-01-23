43cb44b58cb5747f781f35f3fedf5d8e40c5cde7
kelseylw1822 <kelseylw1822@gmail.com>
2025-01-22 21:50:07 -0500

    updated bike

--- Lab1/bikeRidePoints.cpp
+++ Lab1/bikeRidePoints.cpp
@@ -1,8 +1,8 @@
 // My Name: Kelsey Wessel
 // My Class: CPSC 1021
 // Date: 01/22/2025
-// Desc:
-// Time:
+// Desc: Based on user input, prints out the total amount of points they received 
+// Time: About 10 minutes
 
 #include <iostream>
 using namespace std;
@@ -24,32 +24,32 @@ int main()
     if (rides == 0)
     {
         points = 0;
-        cout << "You earned " << points << " this semester";
+        cout << "You earned " << points << " this semester" << endl;
     }
     else if (rides == 1)
     {
         points = 3;
-        cout << "You earned " << points << " this semester";
+        cout << "You earned " << points << " this semester" << endl;
     }
     else if (rides == 2)
     {
         points = 10;
-        cout << "You earned " << points << " this semester";
+        cout << "You earned " << points << " this semester" << endl;
     }
     else if (rides == 3)
     {
         points = 15;
-        cout << "You earned " << points << " this semester";
+        cout << "You earned " << points << " this semester" << endl;
     }
     else if (rides == 4)
     {
         points = 30;
-        cout << "You earned " << points << " this semester";
+        cout << "You earned " << points << " this semester" << endl;
     }
     else if (rides >= 5)
     {
         points = 50;
-        cout << "You earned " << points << " this semester";
+        cout << "You earned " << points << " this semester" << endl;
     }
 
     return 0;
